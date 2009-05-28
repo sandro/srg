517322a69e6b19cb85290ab30569ed5ae0fdcf67
Sandro Turriate <sandro.turriate@gmail.com>
2009-05-28 16:40:03 -0400

    created generator

---
+++ sandro_rails_generator.rb
@@ -0,0 +1,76 @@
+def app_name
+  @app_name ||= File.basename(root)
+end
+
+def app_name_underscore
+  @app_name_underscore ||= app_name.gsub(' ', '_').underscore
+end
+
+FileUtils.rm_rf %w(README public/index.html public/images/rails.png test)
+
+file 'config/routes.rb', <<-END
+ActionController::Routing::Routes.draw do |map|
+end
+END
+
+file 'config/database.yml', <<-END
+development: 
+  adapter: mysql
+  username: root
+  password: 
+  database: #{app_name_underscore}_development
+test: 
+  adapter: mysql
+  username: root
+  password: 
+  database: #{app_name_underscore}_test
+END
+
+file 'db/schema.rb'
+
+#====================
+# GEMS
+#====================
+gem 'giraffesoft-resource_controller', :version => '>= 0.5.5', :lib => 'resource_controller', :source => "http://gems.github.com"
+gem 'mislav-will_paginate', :version => '>= 2.3.7', :lib => 'will_paginate', :source => 'http://gems.github.com'
+gem "haml", :version => ">= 2.0.7"
+gem 'rspec', :version => ">= 1.1.12", :lib => false, :git => 'git://github.com/dchelimsky/rspec.git'
+gem 'rspec-rails', :version => ">= 1.1.12", :lib => false, :git => 'git://github.com/dchelimsky/rspec-rails.git'
+
+#====================
+# APP
+#====================
+
+rake "gems:install", :sudo => true
+rake "db:create:all"
+
+generate("rspec")
+
+run 'haml --rails .'
+rake "log:clear"
+
+#====================
+# GIT
+#====================
+
+git :init
+
+file '.gitignore', <<-END
+.DS_Store
+config/database.yml
+config/settings.yml
+coverage/*
+db/*.db
+db/*.sqlite3
+doc/api
+doc/app
+log/*.log
+tmp/**/*
+tmp/tags
+END
+
+run "cp config/database.yml config/database.example.yml"
+run 'find . \( -type d -empty \) -and \( -not -regex ./\.git.* \) -exec touch {}/.gitignore \;'
+
+git :add => "."
+git :commit => "-a -m 'Generated project'"
